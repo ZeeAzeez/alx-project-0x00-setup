86e91b7eaf6878e46d41bc2aeb3189175f3e52f7
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-16 23:10:39 +0100

    rounded-lg

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -45,15 +45,15 @@ const Landing: React.FC = () => {
 
         <div className="flex gap-4 items-center">
           <Button
-            title="Large Rounded SM"
+            title="Large Rounded SM rounded-lg"
             styles="px-6 py-3 text-lg bg-purple-500 text-white rounded-sm hover:bg-purple-600"
           />
           <Button
-            title="Large Rounded MD"
+            title="Large Rounded MD rounded-lg"
             styles="px-6 py-3 text-lg bg-purple-500 text-white rounded-md hover:bg-purple-600"
           />
           <Button
-            title="Large Rounded Full"
+            title="Large Rounded Full rounded-lg"
             styles="px-6 py-3 text-lg bg-purple-500 text-white rounded-full hover:bg-purple-600"
           />
         </div>
