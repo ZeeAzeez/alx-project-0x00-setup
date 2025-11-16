b7109f35a00abfce156e0473853852626cfef0f3
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-16 23:08:57 +0100

    buttton implementation

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,11 +1,7 @@
 import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
-  return (
-    <button className={styles}>
-      {title}
-    </button>
-  );
+  return <button className={styles}>{title}</button>;
 };
 
 export default Button;
